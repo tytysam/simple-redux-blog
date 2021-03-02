7ea8458761723b2a0a83fa780d8535dd3e0ae344
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-02 16:14:07 -0700

    Initial commit after CRA install and basic configuring.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import PostList from "./PostList.js";
+
+const App = () => {
+  return (
+    <div>
+      <div className="ui container">App</div>
+      <PostList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PostList extends Component {
+  render() {
+    return (
+      <div>
+        <div>Post List</div>
+      </div>
+    );
+  }
+}
+
+export default PostList;
