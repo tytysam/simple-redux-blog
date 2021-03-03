b45396ba8e93d56a1e8a0b7d9b14d7d9592f1841
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-03 12:20:36 -0700

    Added and wired-up usersReducer so that I can attach the author name to each blog post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import jsonPlaceholder from "../apis/jsonPlaceholder.js";
 export const fetchPosts = () => async (dispatch) => {
   const res = await jsonPlaceholder.get("/posts");
 
-  dispatch({ type: "FETCH_POSTS", payload: res });
+  dispatch({ type: "FETCH_POSTS", payload: res.data });
 };
 
 // THE ABOVE IS THE EXACT SAME AS THE BELOW
@@ -14,3 +14,9 @@ export const fetchPosts = () => async (dispatch) => {
 //     dispatch({ type: 'FETCH_POSTS', payload: res})
 //   }
 // };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const res = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: res.data });
+};
