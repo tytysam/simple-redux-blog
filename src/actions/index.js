a4a3283c084d35c29a5ed63403d838e865f36939
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-03 12:48:54 -0700

    Implemented a different memoization technique by finding unique user Ids

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder.js";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const res = await jsonPlaceholder.get("/posts");
 
@@ -16,12 +22,20 @@ export const fetchPosts = () => async (dispatch) => {
 //   }
 // };
 
-export const fetchUser = (id) => (dispatch) => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const res = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: res.data });
-});
+};
+
+// MEMOIZED VERSION
+// * note: The issue with the below method of memoization is that now, if we ever want to retrieve a specific user again, we'd have to create an entirely new arrow function...
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const res = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: res.data });
+// });
