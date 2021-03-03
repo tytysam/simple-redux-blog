21b2e305a87c7c15c6f742c9e4d263b6f3e0d3fa
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-03 12:32:50 -0700

    Added memoization to fix overfetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder.js";
 
 export const fetchPosts = () => async (dispatch) => {
@@ -15,8 +16,12 @@ export const fetchPosts = () => async (dispatch) => {
 //   }
 // };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const res = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: res.data });
-};
+});
