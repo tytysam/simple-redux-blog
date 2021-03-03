0aa3f4ff3d5b13ff375c4dcbf460fdc1ee34ad96
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-03 11:40:34 -0700

    Reconfigured reducer to use a switch statement

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
-export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS",
-  };
+import jsonPlaceholder from "../apis/jsonPlaceholder.js";
+
+export const fetchPosts = () => async (dispatch) => {
+  const res = await jsonPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: res });
 };
+
+// THE ABOVE IS THE EXACT SAME AS THE BELOW
+// export const fetchPosts = () => {
+//   return async function (dispatch, getState) {
+//     const res = await jsonPlaceholder.get('/posts');
+
+//     dispatch({ type: 'FETCH_POSTS', payload: res})
+//   }
+// };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer.js";
 
 export default combineReducers({
-  replaceMeEventually: () => "hi there",
+  posts: postsReducer,
 });
